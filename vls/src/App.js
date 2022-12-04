535feee4bdc55987a446f541b6a94f0b217679c7
Dany serfaty <davidserfaty2315@gmail.com>
2022-12-04 18:54:59 +0200

    init

---
+++ vls/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Nav/>
+      </header>
+        
+      
+    </div>
+  );
+}
+
+export default App;
