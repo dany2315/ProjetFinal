328b0978291869f8bbd5160781d41eb67587250b
Dany serfaty <davidserfaty2315@gmail.com>
2022-12-05 18:35:34 +0200

    beug loading ne s'affiche pas

---
+++ vls/src/components/loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './loading.css';
+
+function Loading () {
+    
+return(
+    <>
+    <div className='loader-container'>
+        <div className="dot d1"></div>
+        <div className="dot d2"></div>
+        <div className="dot d3"></div>
+    </div>
+    </>
+)
+    
+}
+
+export default Loading
